493c7e984c54dec17fc3fcff9f1e51c6bf312119
webos21 <webos21@gmail.com>
2013-09-18 18:15:24 +0900

    update the android sdk to rev.22 (JellyBean 4.3) modify the source tree for android

--- eBookReader/android/src/org/xprojects/ebookreader/MainActivity.java
+++ eBookReader/android/src/org/xprojects/ebookreader/MainActivity.java
@@ -1,4 +1,4 @@
-package org.xprojects.eBookReader;
+package org.xprojects.ebookreader;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
