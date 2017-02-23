d481e10b5a3b81259e96b5f90dd1d39dd3851d72
cmjo <webos21@gmail.com>
2017-02-23 20:46:36 +0900

    * xproject   - add "Life Log" project   - add "Password Book" project

---
+++ PasswordBook/PasswordBook4android/app/src/main/java/com/gmail/webos21/passwordbook/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.webos21.passwordbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
