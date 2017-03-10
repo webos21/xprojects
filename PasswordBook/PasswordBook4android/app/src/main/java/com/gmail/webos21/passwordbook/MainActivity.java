119b4e6490233a195626076a406c3b8a359a6d3d
cmjo <webos21@gmail.com>
2017-03-10 16:11:53 +0900

    * xproject  - PasswordBook : add library

--- PasswordBook/PasswordBook4android/app/src/main/java/com/gmail/webos21/passwordbook/MainActivity.java
+++ PasswordBook/PasswordBook4android/app/src/main/java/com/gmail/webos21/passwordbook/MainActivity.java
@@ -1,8 +1,20 @@
 package com.gmail.webos21.passwordbook;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.gmail.webos21.android.patch.PRNGFixes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +22,63 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean loginFlag;
 
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Android SecureRandom Fix!!! (No Dependency)
+        PRNGFixes.apply();
+
+        // Set Tool-Bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set Drawer-Layout
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer != null) {
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            //noinspection deprecation
+            drawer.setDrawerListener(toggle);
+            toggle.syncState();
+        }
+
+        // Set Navigation-View
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(new NavItemSelected());
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_search:
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -41,6 +106,27 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class NavItemSelected implements NavigationView.OnNavigationItemSelectedListener {
+
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.nav_cur_pos:
+                    break;
+                default:
+                    break;
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            navigationView.getMenu().getItem(0).setChecked(true);
+
+            return true;
+        }
+
     }
 
 }
