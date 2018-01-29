c765fdd163c223d34bea83e5c51a894c906477a9
Cheolmin Jo <webos21@gmail.com>
2018-01-30 01:58:56 +0900

    * OpenCV Project : Initial Commit * PasswordBook4windows : Initial Commit

---
+++ PasswordBook/PasswordBook4windows/PasswordBook4windows/PasswordBook4windows/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace PasswordBook4windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
